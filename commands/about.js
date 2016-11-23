848184bc40ef8c91a08f71b56d829412240e606d
Slorany <sloranymusic@gmail.com>
2016-11-23 01:11:10 +0100

    Add files via upload

--- commands/about.js
+++ commands/about.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'about',
     help: 'info about the bot.',
     process: function (client, message, args, uptime) {
-        message.channel.sendMessage("I am present on " + client.guilds.array().length + " servers.\n**Uptime**: " + uptime + "\n**Created by**: Slorany#6720 \n**My ID is**: " + client.user.id);
+        message.channel.sendMessage("I am present on " + client.guilds.array().length + " servers.\n**Uptime**: " + uptime + "\n**Created by**: Slorany#6720 \n**My ID is**: " + client.user.id + "\n\nYou can find me on trello: <https://github.com/Slorany/LeonardBot>\n And GitHub: <https://github.com/Slorany/LeonardBot>");
     }
 };
--- commands/info.js
+++ commands/info.js
@@ -2,6 +2,5 @@ module.exports = {
     name: 'info',
     help: 'info about the bot.',
     process: function (client, message, args, uptime) {
-        message.channel.sendMessage("I am present on " + client.guilds.array().length + " servers.\n**Uptime**: " + uptime + "\n**Created by**: Slorany#6720 \n**My ID is**: " + client.user.id);
-    }
+        message.channel.sendMessage("I am present on " + client.guilds.array().length + " servers.\n**Uptime**: " + uptime + "\n**Created by**: Slorany#6720 \n**My ID is**: " + client.user.id + "\n\nYou can find me on trello: <https://github.com/Slorany/LeonardBot>\n And GitHub: <https://github.com/Slorany/LeonardBot>");    }
 };
