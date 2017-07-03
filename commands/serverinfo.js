90de7cb26fa1c1b5e269f926ab076543df638019
Slorany <sloranymusic@gmail.com>
2017-07-03 21:47:46 +0200

    Delete serverinfo.js

--- commands/serverinfo.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'serverinfo',
-    help: 'info about the server.',
-    process: function (client, message, args) {
-      var serverCreationDate = message.guild.creationDate;
-      serverCreationDate = serverCreationDate.toISOString().replace(/T/, ' ').replace(/\..+/, '');
-      message.channel.sendMessage("**Server:** " + message.guild.name + "\n**Channel:** <#" + message.channel.id + "> \n" + "This server was created on: " + serverCreationDate);
-    }
-};
