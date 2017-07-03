7a95adb5216aef4d27dfd542bac843f85e81cd89
Slorany <sloranymusic@gmail.com>
2017-07-03 21:47:38 +0200

    Delete say.js

--- commands/say.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'say',
-    help: '`say message` \nMakes the bot send a message containing <message> in the same channel.',
-    process: function (client, message, args) {
-      var text = args.join(" ");
-      if(!text) return message.reply("what the heck do you want me to say?");
-      message.channel.sendMessage(text);
-    }
-};
