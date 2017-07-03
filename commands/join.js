9fdbab267f7766f3e4dac917f71e7e3f61bf29f3
Slorany <sloranymusic@gmail.com>
2017-07-03 21:46:50 +0200

    Delete join.js

--- commands/join.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'say',
-    help: 'Provides the link to add the bot to your server.',
-    process: function (client, message) {
-      const config = require("../config.json");
-      message.reply(config.joinLink);
-    }
-};
